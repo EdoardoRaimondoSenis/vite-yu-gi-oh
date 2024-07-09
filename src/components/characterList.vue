a6c616b2c18864df07c2a302d0ff07a382cf9505
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-07-09 14:01:42 +0200

    Template e file principali creati

---
+++ src/components/characterList.vue
@@ -0,0 +1,21 @@
+<script>
+
+import characterCard from './characterCard.vue'
+
+    export default {
+        name: 'characterList',
+        components: {
+            characterCard,
+        }
+    }
+
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
